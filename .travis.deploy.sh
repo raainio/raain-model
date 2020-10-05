42b302da01d498b622d9146817390a769c6670f8
mlefree <>
2020-10-05 12:03:38 +0200

    travis (1)

---
+++ .travis.deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy."
+  exit 0
+fi
+
+setup_git() {
+    echo "Setup Git."
+    git init
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    git remote -v
+    git remote add upstream "https://${GH_TOKEN}@github.com/raain/raain-model.git"
+
+    REVISION=$(git rev-parse --short HEAD)
+    PACKAGE_VERSION=$(cat package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[", ]//g')
+}
+
+build_version() {
+    echo "Build + Docs + Bump."
+    npm run build
+    # TODO npm run docs
+    npm patch
+}
+
+push_git() {
+    echo "Commit, tag and push on master."
+    git add -A .
+    git commit -m "[ci skip] travis is OK: v${PACKAGE_VERSION} - ${REVISION}"
+    git tag -a "v${PACKAGE_VERSION}" -m "v${PACKAGE_VERSION}"
+    git push -q upstream HEAD:master --tags
+
+}
+
+###########
+## Main
+###########
+
+setup_git
+build_version
+push_git
